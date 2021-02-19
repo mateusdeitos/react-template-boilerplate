4de0e81d1d89ea567b5ebb130877c29ae714a7e7
Mateus Deitos <mate.deitos@hotmail.com>
2021-02-19 13:44:11 -0300

    ajuste

--- src/pages/Cards/DarkMode.tsx
+++ src/pages/Cards/DarkMode.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Card } from '../../components/Card';
 import { useDarkMode } from '../../hooks/useDarkMode';
@@ -6,15 +9,38 @@ import Switch from '../../components/Switch';
 
 const CardDarkMode: React.FC = () => {
   const { isDarkMode, toggleColorMode, currentTheme } = useDarkMode();
-  const [code, setCode] = useState('');
-  useEffect(() => {
-    const str = JSON.stringify(currentTheme, null, 2);
-    const matches = str.match(/#[A-Z0-9]+/gm);
-    if (Array.isArray(matches)) {
-      matches.map(matched => str.replace(matched, `<span>${matched}</span>`));
-    }
-    setCode(str);
+  // const [code, setCode] = useState('');
+  const code = useMemo(() => {
+    const html: string[] = [];
+    const iterate = (_obj: any, _level: number) => {
+      Object.keys(_obj).forEach((key: any) => {
+        const value =
+          typeof _obj[key] === 'object'
+            ? ''
+            : `<label style="background-color: ${_obj[key]}"><span style="color: ${_obj[key]}; filter: invert(100%);">${_obj[key]}</span></label>`;
+        const str = `${'&nbsp&nbsp&nbsp'.repeat(_level)}${key}: ${value}<br/>`;
+        html.push(str);
+        if (typeof _obj[key] === 'object') {
+          _level++;
+          if (key in _obj) {
+            iterate(_obj[key] as Record<string, unknown>, _level);
+          }
+        } else {
+          _level = Math.max(0, _level);
+        }
+      });
+    };
+    iterate(currentTheme, 0);
+    return html.join('<br/>');
   }, [currentTheme]);
+  // useEffect(() => {
+  //   const str = JSON.stringify(currentTheme, null, 2);
+  //   const matches = str.match(/#[A-Z0-9]+/gm);
+  //   if (Array.isArray(matches)) {
+  //     matches.map(matched => str.replace(matched, `<span>${matched}</span>`));
+  //   }
+  //   setCode(str);
+  // }, [currentTheme]);
   return (
     <Card>
       <Container>
@@ -26,9 +52,8 @@ const CardDarkMode: React.FC = () => {
         />
         <hr />
         <p>Current Theme</p>
-        <CodeBlock>
-          <pre>{code}</pre>
-        </CodeBlock>
+        <p>{code}</p>
+        <CodeBlock />
       </Container>
     </Card>
   );
